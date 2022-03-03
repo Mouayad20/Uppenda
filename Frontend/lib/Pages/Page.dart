e6163205a977f90dcfe2eae3b2ebbb10e3f9067b
Mouayad20 <moukad18@gmail.com>
2022-03-04 01:32:12 +0200

    Page Page.

--- Frontend/lib/Pages/Page.dart
+++ Frontend/lib/Pages/Page.dart
@@ -29,7 +29,7 @@ class Page1 extends StatefulWidget {
   @override
   _Page1State createState() => _Page1State();
 }
-
+/////////
 class _Page1State extends State<Page1> {
   PageControler _pageController = PageControler();
   PageModel? _pageModel;
